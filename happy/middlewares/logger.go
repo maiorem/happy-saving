f705e7695588979ae69e2f7aa26472a0615071ab
Seyoung Hong <maiorem00@gmail.com>
2022-09-10 09:18:47 +0900

    logger time Format

--- happy/middlewares/logger.go
+++ happy/middlewares/logger.go
@@ -3,13 +3,14 @@ package middlewares
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"time"
 )
 
 func Logger() gin.HandlerFunc {
 	return gin.LoggerWithFormatter(func(params gin.LogFormatterParams) string {
 		return fmt.Sprintf("%s - [%s] %s %s %d %s\n",
 			params.ClientIP,
-			params.TimeStamp,
+			params.TimeStamp.Format(time.RFC822),
 			params.Method,
 			params.Path,
 			params.StatusCode,
