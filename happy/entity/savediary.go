a31bb546d6426db9493b6a559b4ef4b929700039
Seyoung Hong <maiorem00@gmail.com>
2022-09-12 19:45:30 +0900

    time.Time

--- happy/entity/savediary.go
+++ happy/entity/savediary.go
@@ -5,6 +5,6 @@ import "time"
 type Diary struct {
 	Emoji      string    `json:"emoji"`
 	CreateDate time.Time `json:"createDate"`
-	Title      time.Time `json:"title"`
+	Title      string    `json:"title"`
 	Content    string    `json:"content"`
 }
