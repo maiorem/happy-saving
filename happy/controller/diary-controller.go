c9944a82a7cd50d18ba99bc35ed07ec9091da10c
Seyoung Hong <maiorem00@gmail.com>
2022-10-17 22:56:02 +0900

    diary service and controller

--- happy/controller/diary-controller.go
+++ happy/controller/diary-controller.go
@@ -1,1 +1,37 @@
 package controller
+
+import (
+	"happy/entity"
+	"happy/service"
+
+	"github.com/gin-gonic/gin"
+)
+
+type SaveDiaryController interface {
+	DiaryFindAll() []entity.Diary
+	DiarySave(ctx *gin.Context) error
+}
+
+type diaryController struct {
+	service service.DiaryService
+}
+
+func DiaryNew(service service.DiaryService) SaveDiaryController {
+	return diaryController{
+		service: service,
+	}
+}
+
+func (c diaryController) DiaryFindAll() []entity.Diary {
+	return c.service.FindAll()
+}
+
+func (c diaryController) DiarySave(ctx *gin.Context) error {
+	var diary entity.Diary
+	err := ctx.ShouldBindJSON(&diary)
+	if err != nil {
+		return err
+	}
+	c.service.Save(diary)
+	return nil
+}
--- happy/service/diary-service.go
+++ happy/service/diary-service.go
@@ -1,1 +1,27 @@
 package service
+
+import "happy/entity"
+
+type DiaryService interface {
+	FindAll() []entity.Diary
+	Save(diary entity.Diary) entity.Diary
+	// FindById(diary_id string) entity.Diary
+	// Update(diary entity.Diary)
+}
+
+type diaryService struct {
+	diaries []entity.Diary
+}
+
+func DiaryNew() DiaryService {
+	return &diaryService{}
+}
+
+func (service *diaryService) Save(diary entity.Diary) entity.Diary {
+	service.diaries = append(service.diaries, diary)
+	return diary
+}
+
+func (service *diaryService) FindAll() []entity.Diary {
+	return service.diaries
+}
