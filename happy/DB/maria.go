be0f4741e1104201532de1056637e81f653a698b
Seyoung Hong <maiorem00@gmail.com>
2022-10-21 21:20:16 +0900

    db

--- happy/DB/maria.go
+++ happy/DB/maria.go
@@ -1,1 +1,18 @@
 package db
+
+import "database/sql"
+
+func mariadb() {
+
+	var variable string
+	var setTime string
+
+	db, err := sql.Open("mysql", "maiorem:pass@tcp(localhost:8089)/happysave")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.QueryRow("Select variable, set_time From happy_user").Scan(&variable, &setTime)
+
+}
