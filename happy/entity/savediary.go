a60f3e94c423635c89e9e4e491004630ceb0244f
Seyoung Hong <maiorem00@gmail.com>
2022-10-24 23:01:18 +0900

    gorm model setting

--- happy/entity/savediary.go
+++ happy/entity/savediary.go
@@ -1,13 +1,23 @@
 package entity
 
-import "time"
+import (
+	"time"
+
+	"github.com/jinzhu/gorm"
+)
 
 type Diary struct {
+	gorm.Model
 	ID        uint64    `gorm:"primary_key;auto_increment" json:"id"`
-	Box       SaveBox   `json:"box" binding:"required" gorm:"foreignkey:SaveBoxID"`
 	SaveBoxID uint64    `json:"-"`
-	Emoji     string    `json:"emoji" gorm:"type:varchar(100)"`
-	CreatedAt time.Time `json:"-" gorm:"default:CURRENT_TIMESTAMP" json:"create-date" `
-	UpdatedAt time.Time `json:"-" gorm:"default:CURRENT_TIMESTAMP" json:"update-date"`
+	Emoji     Emoji     `json:"emoji" gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;foreignkey:EmojiID"`
+	EmojiID   uint64    `json:"-"`
+	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"create-date" `
+	UpdatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"update-date"`
 	Content   string    `json:"content" binding:"min=20, max=150" gorm:"type:varchar(150)"`
+}
+type Emoji struct {
+	gorm.Model
+	ID       uint64 `gorm:"primary_key;auto_increment" json:"id"`
+	Emoticon string `gorm:"type:varchar(20)" json:"emoji"`
 }
--- happy/entity/user.go
+++ happy/entity/user.go
@@ -1,12 +1,17 @@
 package entity
 
-import "time"
+import (
+	"time"
+
+	"github.com/jinzhu/gorm"
+)
 
 type User struct {
+	gorm.Model
 	ID       uint64    `gorm:"primary_key;auto_increment" json:"id"`
 	UUID     string    `json:"uuid" binding:"required" gorm:"type:varchar(100)"`
 	Name     string    `json:"name" binding:"required" gorm:"type:varchar(10)"`
 	Email    string    `json:"email" validate:"required, email" gorm:"type:varchar(50)"`
 	JoinDate time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"join-date" `
-	Boxes    []SaveBox `json:"boxes"`
+	Boxes    []SaveBox `json:"boxes" gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;foreignkey:UserID"`
 }
