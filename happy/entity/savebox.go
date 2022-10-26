5591958a0e995a58685605bf495db6bd7a89cc5f
Seyoung Hong <maiorem00@gmail.com>
2022-10-27 07:59:41 +0900

    Update savebox.go

--- happy/entity/savebox.go
+++ happy/entity/savebox.go
@@ -14,5 +14,19 @@ type SaveBox struct {
 	CreateDate  time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"create-date"`
 	OpenDate    time.Time `binding:"required" gorm:"type:timestamp" json:"open-date"`
 	Activate    bool      `json:"activate" gorm:"default:true"`
+	IsOpened    bool      `json:"isopen" gorm:"derault:false"`
 	SaveDiaries []Diary   `json:"save-diaries" gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;foreignkey:SaveBoxID"`
+}
+
+// 다이어리 오픈 여부
+func IsOpenedChange(openDate time.Time, isOpen bool) bool {
+	currentTime := time.Now()
+
+	// 오픈날짜가 아직 안왔으면
+	if openDate.After(currentTime) {
+		isOpen = false
+	} else {
+		isOpen = true
+	}
+	return isOpen
 }
